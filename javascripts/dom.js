a69345fed9ff7e404e35f55598519b88185704f3
Brian Wright <briankw23@gmail.com>
2018-04-18 22:19:52 -0500

    wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -16,7 +16,9 @@ const domOutput = () => {
   let domString = '';
   for (let i = 0; i < categories.length; i++) {
     domString += `<div>`;
-    domString += `<h1>${categories[i].categoryName}</h1>`;
+    domString += `<h3>${categories[i].categoryName}</h3>`;
+    domString += `</div>`;
+    domString += `<div class=".col-md-4">`;
     for (let j = 0; j < movieStuff.length; j++) {
       if (movieStuff[j].categoryId === categories[i].id) {
         domString += `<div class="checkbox disabled">`;
